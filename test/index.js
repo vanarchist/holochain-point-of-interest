a802e046669bc07fe15e36f11110b92975f4d195
vanarchist <v4narchist@gmail.com>
2019-04-19 18:33:37 -0400

    Updated zome names in test case to fix errors

--- test/index.js
+++ test/index.js
@@ -12,8 +12,8 @@ const scenario = new Scenario([instanceAlice])
 scenario.runTape("description of example test", async (t, { alice }) => {
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = alice.call("my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
-  const result = alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
+  const addr = alice.call("point_of_interest", "create_my_entry", {"entry" : {"content":"sample content"}})
+  const result = alice.call("point_of_interest", "get_my_entry", {"address": addr.Ok})
 
   // check for equality of the actual and expected results
   t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })

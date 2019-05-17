854953c8d04facf93fa3f3526070f315f081264e
vanarchist <v4narchist@gmail.com>
2019-05-17 19:57:38 -0400

    Added point saving and loading

--- zomes/point_of_interest/code/src/lib.rs
+++ zomes/point_of_interest/code/src/lib.rs
@@ -7,46 +7,107 @@ extern crate serde_derive;
 extern crate serde_json;
 #[macro_use]
 extern crate holochain_core_types_derive;
+extern crate holochain_collections;
+extern crate geojson;
+extern crate geohash;
+
+use geojson::{Feature, FeatureCollection};
+use geohash::{encode, Coordinate};
 
 use hdk::{
     entry_definition::ValidatingEntryType,
     error::ZomeApiResult,
 };
 use hdk::holochain_core_types::{
-    cas::content::Address, entry::Entry, dna::entry_types::Sharing, error::HolochainError, json::JsonString,
-    validation::EntryValidationData
+    cas::content::Address, dna::entry_types::Sharing,
+    error::HolochainError, json::JsonString
+};
+use holochain_collections::bucket_set::{
+    self,
+    BucketSetStorable,
+    BucketIterable,
 };
 
-// see https://developer.holochain.org/api/0.0.10-alpha2/hdk/ for info on using the hdk library
+// Use newtype idiom for geojson::Feature to implement DefaultJson
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
+pub struct PointOfInterest(Feature);
 
-// This is a sample zome that defines an entry type "MyEntry" that can be committed to the
-// agent's chain via the exposed function create_my_entry
-
-#[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)]
-pub struct MyEntry {
-    content: String,
+// Implement traits for holochain-collections
+// Generate geohash bucket id for given point
+// Use 1 character geohash which gives 32 buckets
+impl BucketSetStorable for PointOfInterest {
+    fn derive_bucket_id(&self) -> String {
+        // get point data out of PointOfInterest
+        let value = self.to_owned().0.geometry.unwrap().value;
+        match value {
+            // If we were given a point geometry
+            geojson::Value::Point(value) => {
+                println!("value: {:?}", value[0]);
+                // encode lat/lng to geohash
+                let c = Coordinate { x: value[0], y: value[1] };
+                // use first char of geohash as bucket index (32 buckets)
+                let hash = encode(c, 1).expect("Invalid coordinate");
+                hash
+            },
+            // Anything else we can't handle
+            _ => unreachable!(),
+        }
+    }
 }
 
-pub fn handle_create_my_entry(entry: MyEntry) -> ZomeApiResult<Address> {
-    let entry = Entry::App("my_entry".into(), entry.into());
-    let address = hdk::commit_entry(&entry)?;
-    Ok(address)
+// All bucket ids which is the 32 character geohash alphabet
+impl BucketIterable for PointOfInterest {
+    fn buckets() -> Box<Iterator<Item = String>> {
+        // use base32 geohash alphabet
+        let alphabet = "0123456789bcdefghjkmnpqrstuvwxyz"
+        .chars().map(|c| {
+            c.to_string()
+        });
+        Box::new(alphabet)
+    }
 }
 
-pub fn handle_get_my_entry(address: Address) -> ZomeApiResult<Option<Entry>> {
-    hdk::get_entry(&address)
+// Use newtype idiom for geojson::FeatureCollection to implement DefaultJson
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
+pub struct PointsOfInterest(FeatureCollection);
+
+// save point to bucket based on geographic location
+pub fn handle_create_point_of_interest(entry: PointOfInterest) -> ZomeApiResult<Address> {
+    bucket_set::store("point_of_interest".into(), entry)
+}
+
+// return a wrapped geojson::FeatureCollection of all points to client
+pub fn handle_load_points() -> ZomeApiResult<PointsOfInterest> {
+
+    let points = bucket_set::retrieve_all::<PointOfInterest>("point_of_interest".into()).unwrap();
+    let mut features: Vec<Feature> = Vec::new();
+
+    for p in points {
+        let point = hdk::utils::get_as_type::<PointOfInterest>(p.to_owned())?;
+        let feature = point.0;
+        features.push(feature);
+    }
+    
+    let collection = FeatureCollection {
+        bbox: None,
+        features: features,
+        foreign_members: None,
+    };
+    
+    let poi = PointsOfInterest(collection);
+    Ok(poi)
 }
 
 fn definition() -> ValidatingEntryType {
     entry!(
-        name: "my_entry",
-        description: "this is a same entry defintion",
+        name: "point_of_interest",
+        description: "point of interest information",
         sharing: Sharing::Public,
         validation_package: || {
             hdk::ValidationPackageDefinition::Entry
         },
 
-        validation: | _validation_data: hdk::EntryValidationData<MyEntry>| {
+        validation: | _validation_data: hdk::EntryValidationData<PointOfInterest>| {
             Ok(())
         }
     )
@@ -54,25 +115,26 @@ fn definition() -> ValidatingEntryType {
 
 define_zome! {
     entries: [
-       definition()
+       definition(),
+       bucket_set::bucket_entry_def_for("point_of_interest".into())
     ]
 
     genesis: || { Ok(()) }
 
     functions: [
-        create_my_entry: {
-            inputs: |entry: MyEntry|,
+        create_point_of_interest: {
+            inputs: |entry: PointOfInterest|,
             outputs: |result: ZomeApiResult<Address>|,
-            handler: handle_create_my_entry
+            handler: handle_create_point_of_interest
         }
-        get_my_entry: {
-            inputs: |address: Address|,
-            outputs: |result: ZomeApiResult<Option<Entry>>|,
-            handler: handle_get_my_entry
+        load_points: {
+            inputs: | |,
+            outputs: |result: ZomeApiResult<PointsOfInterest>|,
+            handler: handle_load_points
         }
     ]
 
     traits: {
-        hc_public [create_my_entry,get_my_entry]
+        hc_public [load_points, create_point_of_interest]
     }
 }

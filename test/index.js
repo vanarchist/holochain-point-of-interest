750177b644e91b256b69723d5cc045e83aff6cd3
vanarchist <v4narchist@gmail.com>
2019-05-21 13:55:12 -0400

    Updated tests

--- test/index.js
+++ test/index.js
@@ -5,16 +5,33 @@ Scenario.setTape(require("tape"))
 
 const dnaPath = "./dist/holochain-point-of-interest.dna.json"
 const agentAlice = Config.agent("alice")
+const agentBob = Config.agent("bob")
 const dna = Config.dna(dnaPath)
 const instanceAlice = Config.instance(agentAlice, dna)
-const scenario = new Scenario([instanceAlice])
+const instanceBob = Config.instance(agentBob, dna)
+const scenario = new Scenario([instanceAlice, instanceBob], { debugLog: false })
 
-scenario.runTape("description of example test", async (t, { alice }) => {
-  // Make a call to a Zome function
-  // indicating the function, and passing it an input
-  const addr = alice.call("point_of_interest", "create_my_entry", {"entry" : {"content":"sample content"}})
-  const result = alice.call("point_of_interest", "get_my_entry", {"address": addr.Ok})
+scenario.runTape("point of interest demo tests", async (t, { alice, bob }) => {
 
-  // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
+    // No points at initial run of app returns empty FeatureCollection
+    const result = await alice.callSync("point_of_interest", "get_all_points", {"args":{}})
+    // Make sure returns empty geojson FeatureCollection
+    t.deepEqual(result, { Ok: {"type":"FeatureCollection", "features":{}} })
+
+    // Make sure that node sees other node points
+    var aliceTestPoint = {
+            "type": "Feature",
+            "properties": {
+                "name": "Alice test point"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-114.5, 34.5]
+            }};
+    const aliceAddr = await alice.callSync("point_of_interest", "add_point", {"entry" : aliceTestPoint})
+    const aliceResult = await alice.callSync("point_of_interest", "get_all_points", {"args":{}})
+    t.deepEqual(aliceResult, { Ok: {"type":"FeatureCollection", "features":[aliceTestPoint]} })
+
+    const bobResult = await bob.callSync("point_of_interest", "get_all_points", {"args":{}})
+    t.deepEqual(bobResult, { Ok: {"type":"FeatureCollection", "features":[aliceTestPoint]} })
 })
